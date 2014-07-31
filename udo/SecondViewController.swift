aa863ab82f36a313d4b9ab0b8982d513e238fd42
Osman A <osman.alpay@gmail.com>
2014-07-31 20:43:30 +0300

    Initial Commit

---
+++ udo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  udo
+//
+//  Created by Osman Alpay on 31/07/14.
+//  Copyright (c) 2014 Osman Alpay. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
